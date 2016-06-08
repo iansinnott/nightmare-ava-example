a075de2eee0b0a1fba901d9f03a8c4571ee14c7b
Ian Sinnott <ian@iansinnott.com>
2016-06-07 23:31:07 -0700

    Run tests serially to domonstrate async tests with single Nightmare instance

--- test.js
+++ test.js
@@ -3,7 +3,18 @@ import Nightmare from 'nightmare';
 
 const nightmare = Nightmare({ show: true });
 
-test(function * (t) {
+/**
+ * NOTE: Tests are run serially (as opposed to the default of concurrently)
+ * because otherwise errors are thrown. I'm not clear on why just yet, but it
+ * may be something to do with using only a single instance of Nightmare for all
+ * concurrent tests.
+ *
+ * This problem can be solved by creating multiple instances of Nightmare in
+ * each concurrent tests. For our purposes here I chose to demonstrate serial
+ * tests instead, which do allow us to use only one Nightmare instance.
+ */
+
+test.serial('Generators!', function * (t) {
   const result = yield nightmare
     .goto('http://yahoo.com')
     .type('form[action*="/search"] [name=p]', 'github nightmare')
@@ -11,8 +22,20 @@ test(function * (t) {
     .wait('#main')
     .evaluate(function () {
       return document.querySelector('#main .searchCenterMiddle li a').href;
-    })
-    .end();
+    });
+
+  t.true(result.includes('images.search.yahoo.com'));
+});
+
+test.serial('Async/Await!', async t => {
+  const result = await nightmare
+    .goto('http://yahoo.com')
+    .type('form[action*="/search"] [name=p]', 'github nightmare')
+    .click('form[action*="/search"] [type=submit]')
+    .wait('#main')
+    .evaluate(function () {
+      return document.querySelector('#main .searchCenterMiddle li a').href;
+    });
 
   t.true(result.includes('images.search.yahoo.com'));
 });
